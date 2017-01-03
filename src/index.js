fb39c402cd62fdddd21d25deb4e9d1223d3fdb8c
Deonis Peretyagin <karudo@gmail.com>
2017-01-03 11:51:46 +0700

    tabs to spaces

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import './style';
 
 let root;
 function init() {
-	let App = require('./components/app').default;
-	root = render(<App />, document.body, root);
+  let App = require('./components/app').default;
+  root = render(<App />, document.body, root);
 }
 
 // in development, set up HMR:
 if (module.hot) {
-	//require('preact/devtools');   // turn this on if you want to enable React DevTools!
-	module.hot.accept('./components/app', () => requestAnimationFrame(init) );
+  //require('preact/devtools');   // turn this on if you want to enable React DevTools!
+  module.hot.accept('./components/app', () => requestAnimationFrame(init) );
 }
 
 init();
